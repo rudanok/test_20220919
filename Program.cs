dbff7883404065fa591a6fc67c3c6b56afe835fc
rudanok <a.rudanok@gmail.com>
2022-09-19 21:10:59 +0300

    Создана программа в файле Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string [] sourceArray = new string [4] {"hello", "2", "world", ":-)"};
+string [] resultArray = new string [sourceArray.Length];
+
+void FindElements(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FindElements(sourceArray, resultArray);
+PrintArray(resultArray);
